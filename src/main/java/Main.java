5c64c2a01c9d48c2d80e4991ec3a76ad6dd927b3
Tim van der Graaff <timvdg45@gmail.com>
2020-09-19 13:52:38 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Object[] sources = new Object[4];
+        Object[] sources = new Object[5];
         
         sources[0] = Directory_Hider.class;
         sources[1] = apps.Admin_Change_User_Status.class;
 		sources[2] = apps.Admin_Change_Users.class;
 		sources[3] = apps.Admin_Chat_Interface.class;
+		sources[4] = apps.Admin_Create_Message.class;
 		
         SpringApplication.run(sources, args);
     }
