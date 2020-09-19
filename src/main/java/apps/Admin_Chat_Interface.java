cb898a98e6b1d86fded8e3828c1b57313131bf98
Tim van der Graaff <timvdg45@gmail.com>
2020-09-19 12:06:41 -0500

    Update Admin_Chat_Interface.java

--- src/main/java/apps/Admin_Chat_Interface.java
+++ src/main/java/apps/Admin_Chat_Interface.java
@@ -61,7 +61,7 @@ public class Admin_Chat_Interface extends HttpServlet {
     */
     
     @Override
-	@PostMapping("/admin-chat-interface")
+	@GetMapping("/admin-chat-interface")
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         processRequest(request, response);
