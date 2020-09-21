35b29e28813b2ea8048da0f8fa2dbd69573c3921
Tim van der Graaff <timvdg45@gmail.com>
2020-09-21 09:03:22 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Object[] sources = new Object[8];
+        Object[] sources = new Object[15];
         
         sources[0] = Directory_Hider.class;
         sources[1] = apps.Admin_Change_User_Status.class;
@@ -16,6 +16,7 @@ public class Main {
 		sources[5] = apps.Admin_Delete_My_Messages.class;
 		sources[6] = apps.Admin_Extract_Messages.class;
 		sources[7] = apps.Admin_Search_Messages.class;
+		sources[8] = apps.Change_User_Status.class;
 		
         SpringApplication.run(sources, args);
     }
