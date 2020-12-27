bc5e58275ade3312eef372dc4a19fceee4daa6ad
Tim van der Graaff <timvdg45@gmail.com>
2020-12-27 13:05:17 -0600

    Update Chat_Interface.java

--- src/main/java/apps/Chat_Interface.java
+++ src/main/java/apps/Chat_Interface.java
@@ -45,7 +45,7 @@ public class Chat_Interface extends HttpServlet {
         processRequest(request, response);
         
         response.setContentType("text/html");
-        response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
+        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
